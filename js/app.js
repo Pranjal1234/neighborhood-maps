8b291f68e3397ac1c1afdbf019b3f225bb39d0b3
Pranjal Patel <pranjalpatelpp@gmail.com>
2017-11-07 02:04:45 -0500

    Search text working and markers laid.

--- js/app.js
+++ js/app.js
@@ -7,27 +7,65 @@ var intLocations = [
   {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
 
-var Location = function(data) {
-	this.title = ko.observable(data.title);
-	this.lat = ko.observable(data.location.lat);
-	this.lng = ko.observable(data.location.lng);
+function populateInfoWindow(marker,infowindow) {
+	if (infowindow.marker != marker) {
+		infowindow.setContent('Hello');
+		infowindow.marker = marker;
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+
+		infowindow.open(map,marker)
+	}
 }
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7413549, lng: -73.9980244},
-      zoom: 13,
-      mapTypeControl: false
-    });
+var largeInfowindow = new google.maps.InfoWindow();
+
+var Location = function(data,marker) {
+	var self = this;
+
+	this.title = ko.observable(data.title);
+	this.location = ko.observable(data.location);
+	this.marker = marker;
+
+	self.marker.addListener('click', function() {
+		populateInfoWindow(self.marker, largeInfowindow);
+	});
 }
+
+
+
 
 var ViewModel = function() {
 	var self = this;
 
-	this.locations = ko.observableArray([]);
+	this.map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 40.7413549, lng: -73.9980244},
+	  zoom: 13,
+	});
 
+	this.locations = ko.observableArray([]);
+	this.searchTerm = ko.observable("");
+
+	this.searchList = ko.computed(function(){
+		var searchResult = [];
+		self.locations().forEach(function(location) {
+			if (location.title().toLowerCase().indexOf(self.searchTerm().toLowerCase()) >= 0) {
+				location.marker.setMap(self.map);
+				searchResult.push(location);
+			} else {
+				location.marker.setMap(null);
+			}
+		})
+		return searchResult
+	},this);
+	
 	intLocations.forEach(function(location) {
-		self.locations.push(new Location(location))
+		self.locations.push(new Location(location, new google.maps.Marker({
+			position: location.location,
+			title: location.title
+		})));
 	});
 
 
@@ -35,3 +73,4 @@ var ViewModel = function() {
 }
 
 ko.applyBindings(new ViewModel());
+
